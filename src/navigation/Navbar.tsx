8740e8bd3f0debcecc1016d19c636fbcf27af9a2
Luca Liam Schäfer <luca.schaefer98@gmx.de>
2023-09-07 21:56:40 +0200

    adding color theme to tailwind config, adjust navbar styling and links

--- src/navigation/Navbar.tsx
+++ src/navigation/Navbar.tsx
@@ -1,32 +1,59 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import "./navbar.css";
 
 function Navbar() {
+  let location = useLocation();
   return (
-    <nav className="tw-relative tw-flex tw-flex-wrap tw-items-center tw-justify-between tw-px-2 tw-py-3 tw-bg-slate-500">
-      <ul className="tw-list-none tw-flex tw-flex-wrap">
+    <nav className="tw-relative tw-flex tw-flex-wrap tw-items-center tw-justify-between tw-px-2 tw-py-3 tw-bg-primary">
+      <ul className="tw-list-none tw-flex tw-flex-wrap tw-space-x-6">
         <li>
-          <Link className="tw-no-underline" to="/">
+          <Link
+            className={`${
+              location.pathname === "/" ? "active" : ""
+            } tw-no-underline tw-text-tertiary nav-link-hover`}
+            to="/"
+          >
             Home
           </Link>
         </li>
         <li>
-          <Link className="tw-no-underline" to="/street-portfolio">
+          <Link
+            className={`${
+              location.pathname === "/street-portfolio" ? "active" : ""
+            } tw-no-underline tw-text-tertiary nav-link-hover`}
+            to="/street-portfolio"
+          >
             Street & Urban
           </Link>
         </li>
         <li>
-          <Link className="tw-no-underline" to="/wild-life-portfolio">
+          <Link
+            className={`${
+              location.pathname === "/wild-life-portfolio" ? "active" : ""
+            } tw-no-underline tw-text-tertiary nav-link-hover`}
+            to="/wild-life-portfolio"
+          >
             Wild Life
           </Link>
         </li>
         <li>
-          <Link className="tw-no-underline" to="/nature-forest-portfolio">
+          <Link
+            className={`${
+              location.pathname === "/nature-forest-portfolio" ? "active" : ""
+            } tw-no-underline tw-text-tertiary nav-link-hover`}
+            to="/nature-forest-portfolio"
+          >
             Nature & Forest
           </Link>
         </li>
         <li>
-          <Link className="tw-no-underline" to="/landscape-portfolio">
+          <Link
+            className={`${
+              location.pathname === "/landscape-portfolio" ? "active" : ""
+            } tw-no-underline tw-text-tertiary nav-link-hover`}
+            to="/landscape-portfolio"
+          >
             Landscapes
           </Link>
         </li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
   prefix: "tw-",
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      background: "#040D12",
+      primary: "#183D3D",
+      accent: "#5C8374",
+      tertiary: "#93B1A6",
+    },
   },
   corePlugins: {
     preflight: false,
